0536160117eeefd2c740ace344c1e5a53ddfec7b
husamahmud <husamahmud@gmail.com>
2024-06-09 05:23:11 +0300

    feat: Implement `Create new note cmd`

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
+  .command('new <note>', 'Create a new note', yargs => {
+    return yargs.positional('note', {
+      type: 'string',
+      description: 'The content of note to create',
+    })
+  }, (argv) => {
+    console.log(argv.note)
+  })
+  .option('tags', {
+    alias: 't',
+    type: 'string',
+    description: 'tags to add to the note',
+  })
   .demandCommand(1)
   .parse()
